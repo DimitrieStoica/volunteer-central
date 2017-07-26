f1e2787c2108212f01b9acee07e793f0df5d6677
Alex Stoica <dimitrie.a.stoica@tsgforce.com>
2017-07-26 19:41:18 +0100

    changed model for frontend

--- src/main/java/com/demo/volunteercentral/pojo/Ad.java
+++ src/main/java/com/demo/volunteercentral/pojo/Ad.java
@@ -2,9 +2,10 @@ package com.demo.volunteercentral.pojo;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.Date;
 
 @Entity
-@Table(name = "add")
+@Table(name = "ad")
 public class Ad {
 
     @Id
@@ -14,12 +15,31 @@ public class Ad {
     @NotNull
     private String goal;
 
+    @NotNull
+    private Integer requestedNumber;
+
+    @NotNull
+    private String location;
+
+    @NotNull
+    private String author;
+
+    @NotNull
+    private Date date;
+
+    @NotNull
+    private String[] skills;
+
     public Ad() {
     }
 
-    public Ad(String id, String goal) {
-        this.id = id;
+    public Ad(String goal, Integer requestedNumber, String location, String author, Date date, String[] skills) {
         this.goal = goal;
+        this.requestedNumber = requestedNumber;
+        this.location = location;
+        this.author = author;
+        this.date = date;
+        this.skills = skills;
     }
 
     public String getId() {
@@ -36,5 +56,45 @@ public class Ad {
 
     public void setGoal(String goal) {
         this.goal = goal;
+    }
+
+    public Integer getRequestedNumber() {
+        return requestedNumber;
+    }
+
+    public void setRequestedNumber(Integer requestedNumber) {
+        this.requestedNumber = requestedNumber;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String[] getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String[] skills) {
+        this.skills = skills;
     }
 }
